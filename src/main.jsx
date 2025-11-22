275e4be3cee3a642bcea69cd0cb5ddc8400b3f4d
Sahil Saini <sahilsainimani8398@gmail.com>
2025-11-22 16:15:44 +0530

    analytics installed

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <SpeedInsights />
+      <Analytics/>
       <App />
     </BrowserRouter>
   </StrictMode>
